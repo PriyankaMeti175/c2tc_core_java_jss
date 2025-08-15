e5f9628e537f8bb1c4dd744f53f4776acb1657ed
PriyankaMeti175 <priyankameti15@gmail.com>
2025-08-15 18:54:19 +0530

    Add files via upload

---
+++ Polymorphism/src/com/tnsif/polymorphism/Calculator.java
@@ -0,0 +1,13 @@
+package com.tnsif.polymorphism;
+
+public class Calculator {
+
+}
+public Calculator() {
+	
+}
+public Calculator(int numOne,int numTwo) {
+	this.numOne=numOne;
+	this.numTwo=numTwo;
+	
+}
---
+++ com.tnsif.JavaBasics/src/com/tnsif/inheritence/Main.java
@@ -0,0 +1,21 @@
+package com.tnsif.inheritence;
+
+public class Main {
+	public static void main(String[] args) {
+		City city=new City();
+		city.setCityName("nashik");
+		city.setArea(264.2f);
+		city.setStateName("karnataka");
+		city.setLanguage("kannada");
+		city.setCountryName("India");
+		city.setCapital("Bangalore");
+		System.out.println(city);
+		
+		
+	}
+	
+	
+	
+	
+
+}
